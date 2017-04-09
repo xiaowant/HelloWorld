5a845d4abf9258735b158314f8e892c5a42902f3
deanhu <huwangcheng1985@163.com>
2017-04-09 11:38:07 +0800

    Hello World Initial commit

---
+++ src/com/dean/demo/Helloworld.java
@@ -0,0 +1,11 @@
+package com.dean.demo;
+
+/**
+ * Created by Dean on 2017/4/9.
+ */
+public class Helloworld {
+    public static void main(String[] args){
+        System.out.println("HelloWorld 20170409");
+        System.out.println("This is a change for git vcs");
+    }
+}
